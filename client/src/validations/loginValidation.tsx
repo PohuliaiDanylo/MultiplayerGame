a0849c56d0ac223274bb6571ee0cbb7fed253956
MrGeelona <mr.geelona@gmail.com>
2025-03-17 13:14:35 +0100

    simplified the code in each validation function

--- client/src/validations/loginValidation.tsx
+++ client/src/validations/loginValidation.tsx
@@ -1,9 +1,6 @@
 import * as Yup from "yup";
 
-export const loginValidation = () => {
-    const loginSchema = Yup.object().shape({
-        username: Yup.string().required("This field is required"),
-        password: Yup.string().required("This field is required"),
-    });
-    return loginSchema;
-};
+export const loginValidation = Yup.object().shape({
+    username: Yup.string().required("This field is required"),
+    password: Yup.string().required("This field is required"),
+});
